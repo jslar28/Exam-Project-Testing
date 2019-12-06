cde2b588dd2426579d7d0e742bd0c23a287059fb
Jakob Larsen <jslar28@gmail.com>
2019-12-06 15:35:22 +0100

    SDK test

--- src/main/java/exam/project/ProjectApplication.java
+++ src/main/java/exam/project/ProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectApplication {
     public static void main(String[] args) {
+        System.out.println("Hello");
         SpringApplication.run(ProjectApplication.class, args);
     }
 }
